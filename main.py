113aedd14c265e956049386547efb402a8ac4095
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-05-20 00:54:42 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# INSTRUCTIONS FOR OAUTH:
+# To begin OAuth process, visit: https://laig493oauth.appspot.com
 
 # [START app]
 import logging
 import requests
-import os
+# import os
 import json
 
 # [START imports]
@@ -28,12 +17,12 @@ from flask import Flask, render_template, request, redirect
 # [END imports]
 
 # Constants
-CLIENT_ID = "183048715948-mlej4766rra4liina1pct9ei1ll8cks0.apps.googleusercontent.com"
-STATE = "mNWCUc-h4Igc7ryHx2q6hJXj"
+CLIENT_ID = "992657165437-lnd8nihet3be0ru71d6nrkle8e8kt9i2.apps.googleusercontent.com"
+STATE = "U4DrDzq0ewPbKEZpmcVoc_X6"
 PREFIX = "https://accounts.google.com/o/oauth2/v2/auth"
 POST_PREFIX = "https://www.googleapis.com/oauth2/v4/token"
 # REDIRECT_URI = "http://localhost:5000/submitted"
-REDIRECT_URI = "https://laig493.appspot.com/submitted"
+REDIRECT_URI = "https://laig493oauth.appspot.com/submitted"
 DYNAMIC_PARAM = "555"
 
 # [START create_app]
@@ -82,7 +71,7 @@ def redirect_auth():
 
 
 
-    return('',200)
+
 # [START form]
 @app.route('/form'+DYNAMIC_PARAM)
 def form():
